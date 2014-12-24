15a600c1608378521174eebee896ba2b74245e26
Teio2810 <dona.293@gmail.com>
2014-12-24 20:58:39 +0200

    Update tests.java

--- amar/src/Tests/tests.java
+++ amar/src/Tests/tests.java
@@ -1,14 +1,52 @@
 package Tests;
-
 import static org.junit.Assert.*;
+
+import java.util.List;
 
 import org.junit.Test;
 
-public class tests {
+import Model.Deck;
+import Model.Game;
 
+
+public class Tests {
+
+	Game game = new Game();
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testShuffle() {
+		Deck deck1 = new Deck();
+	    Deck deck2 = new Deck();
+	    game.shuffle(deck2);
+	   
+	    assert false == deck1.equals(deck2);
 	}
 
+	@Test
+	public void testUpdateHandPlayer() {
+		int sum = game.getPlayerHand().getSum();
+		game.updateHandPlayer(5);
+		int updatedSum = game.getPlayerHand().getSum();
+		int diff = updatedSum - sum;
+		
+		assert true == (sum == (diff));
+	}
+
+	@Test
+	public void testUpdateHandDealer() {
+		int sum = game.getDealerHand().getSum();
+		game.updateHandDealer(5);
+		int updatedSum = game.getDealerHand().getSum();
+		int diff = updatedSum - sum;
+		
+		assert true == (sum == (diff));
+	}
+
+	@Test
+	public void testCreateHitLogic() {
+		List<String> cards = game.getPlayerHand().getCards();
+		String card = game.createHitLogic();
+		List<String> updatedCards = game.getPlayerHand().getCards();
+		
+		assert true == (!cards.contains(card) && updatedCards.contains(card));
+	}
 }
